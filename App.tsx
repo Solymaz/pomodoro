672b0bc1fc95dda4466815f341299f019122e2a5
Solmaz <solmaz.mousavie@gmail.com>
2023-05-15 22:43:03 +0200

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,11 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
+
+const FOCUS_TIME_MINUTES = 0.2 * 60 * 1000;
+const BREAK_TIME_MINUTES = 0.2 * 60 * 1000;
 
 export default function App() {
+  const [timerCount, setTimerCount] = useState(FOCUS_TIME_MINUTES);
+  const startTimer = () => {
+    setInterval(() => setTimerCount((prev) => prev - 1000), 1000);
+  };
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Button title="Start timer" onPress={() => startTimer()} />
+      <Text>{timerCount}</Text>
     </View>
   );
 }
@@ -13,8 +22,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
