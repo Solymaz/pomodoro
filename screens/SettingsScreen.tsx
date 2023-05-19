451aab83fbe0a875ef433e189c5592b650e699b5
Solmaz <solmaz.mousavie@gmail.com>
2023-05-19 23:51:20 +0200

    settings form (work in progress)

--- screens/SettingsScreen.tsx
+++ screens/SettingsScreen.tsx
@@ -1,6 +1,25 @@
-import React from "react";
-import { Text } from "react-native";
+import React, { useState } from "react";
+import { Button, Text, TextInput } from "react-native";
 
 export default function SettingsScreen(): JSX.Element {
-  return <Text>Settings</Text>;
+  const [focusMinutes, setFocusMinutes] = useState<number>(20);
+  const [breakMinutes, setBreakMinutes] = useState<number>(5);
+  return (
+    <>
+      <Text>Enter the number of minutes you want to focus</Text>
+      <TextInput
+        value={focusMinutes}
+        onChangeText={(newTime) => setFocusMinutes(newTime)}
+      />
+      <Text>Enter the number of minutes you want to take a break</Text>
+      <TextInput
+        value={breakMinutes}
+        onChangeText={(newTime) => setFocusMinutes(newTime)}
+      />
+      <Button
+        title="Save"
+        onPress={() => onSumit(focusMinutes, breakMinutes)}
+      />
+    </>
+  );
 }
